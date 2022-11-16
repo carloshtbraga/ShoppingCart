7e3216306c13a1f0fed4afe96e5b49a6d7dcebc3
Carlos <carloshtbraga@gmail.com>
2022-11-16 17:56:24 -0300

    alex3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const oi = await fetchProductsList('computador');
+
+oi.forEach((ele) => {
+  const um = createProductElement(ele);
+  const dois = document.querySelector('.products');
+  dois.appendChild(um);
+});
