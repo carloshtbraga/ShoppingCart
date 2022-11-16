a4b8b7d62d27a951116d5a424ccba7ab6465bc39
Carlos <carloshtbraga@gmail.com>
2022-11-16 16:23:06 -0300

    req 1 e 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -5,8 +5,8 @@ export const fetchProduct = () => {
 const url = 'https://api.mercadolibre.com/sites/MLB/search?q=';
 
 export const fetchProductsList = async (para) => {
+  if (!para) throw new Error('Termo de busca não informado');
   try {
-    if (!para) throw new Error('Termo de busca não informado');
     const um = await fetch(url + para);
     const dados = await um.json();
     return dados.results;
