83862f4b4221b4ef4e681c3a7cb2023b56e4c5a1
Carlos <carloshtbraga@gmail.com>
2022-11-17 01:28:29 -0300

    req11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,24 @@
+const cepButton = document.getElementById('cep');
+const url = 'https://cep.awesomeapi.com.br/json/';
+const url2 = 'https://brasilapi.com.br/api/cep/v2/';
+const input = document.getElementById('input');
+const adr = document.querySelector('.cart__address');
+
+function erro() {
+  adr.innerHTML = 'CEP não encontrado';
+}
+
 export const getAddress = () => {
-  // seu código aqui
+  Promise.any([fetch(url + input.value),
+    fetch(url2 + input.value)]).then((res) => res.json()).then((dois) => {
+    adr.innerHTML = `${dois.address} - ${dois.district} - ${dois.city} - ${dois.state}`;
+  });
 };
 
 export const searchCep = () => {
-  // seu código aqui
+  if (input.value === '00000000') {
+    erro();
+  } if (input.value !== '00000000') getAddress();
 };
+
+cepButton.addEventListener('click', searchCep);
