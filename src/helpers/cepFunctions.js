4b91f71a17b93d72876f1924c82edecaed5c22e1
Carlos <carloshtbraga@gmail.com>
2022-11-18 21:10:10 -0300

    req8

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -8,17 +8,27 @@ function erro() {
   adr.innerHTML = 'CEP não encontrado';
 }
 
-export const getAddress = () => {
-  Promise.any([fetch(url + input.value),
-    fetch(url2 + input.value)]).then((res) => res.json()).then((dois) => {
-    adr.innerHTML = `${dois.address} - ${dois.district} - ${dois.city} - ${dois.state}`;
-  });
+export const getAddress = async () => {
+  // Promise.any([fetch(url + input.value),
+  // fetch(url2 + input.value)]).then((res) => res.json()).then((dois) => {
+  // adr.innerHTML = `${dois.address} - ${dois.district} - ${dois.city} - ${dois.state}`;
+  // });
+  const um = url + input.value;
+  const doiss = url2 + input.value;
+  const retorno = await Promise.any([fetch(um), fetch(doiss)]);
+  const dois = await retorno.json();
+  const address = dois.address || dois.street;
+  const district = dois.district || dois.neighborhood;
+  adr.innerHTML = `${address} - ${district} - ${dois.city} - ${dois.state}`;
+  if (dois.status) {
+    erro();
+  }
 };
 
 export const searchCep = () => {
-  if (input.value === '00000000') {
+  if (!Number(input.value)) {
     erro();
-  } if (input.value !== '00000000') getAddress();
+  } else getAddress();
 };
 
 cepButton.addEventListener('click', searchCep);
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
-import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
+import { createCartProductElement, createProductElement } from './helpers/shopFunctions';
+import { getSavedCartIDs, saveCartID } from './helpers/cartFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
@@ -35,5 +36,13 @@ const funcaoCriaElementos = async (para) => {
     div.appendChild(sunda);
   }
 };
-
 await funcaoCriaElementos('computador');
+
+const botao = document.querySelectorAll('.product__add');
+const olpai = document.querySelector('.cart__products');
+botao.forEach((element) => element.addEventListener('click', async (event) => {
+  const alvo = event.target.parentNode.firstChild.innerText;
+  saveCartID(alvo);
+  const primeiro = await fetchProduct(alvo);
+  olpai.appendChild(createCartProductElement(primeiro));
+}));
